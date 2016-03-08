7ea2767e77f697c8c95693e0949064a81850523a
Stan <satanwang@163.com>
2016-03-08 16:31:10 +0800

    GenerateParenthesis

---
+++ src/com/stan/test/GenerateParenthesis.java
@@ -0,0 +1,32 @@
+package com.stan.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定 n 对括号，请写一个函数以将其生成新的括号组合，并返回所有组合结果。
+ * <p>
+ * Created by xdja on 2016/3/8.
+ */
+public class GenerateParenthesis {
+
+    public ArrayList<String> generateParenthesis(int n) {
+        ArrayList<String> ls = new ArrayList<>();
+        if (n <= 0)
+            return ls;
+        Gen(n, n, "", ls);
+
+        return ls;
+    }
+
+    public static void Gen(int l, int r, String s, List<String> ls) {
+        if (r < l)
+            return;
+        if (l == 0 && r == 0)
+            ls.add(s);
+        if (l > 0)
+            Gen(l - 1, r, s + '(', ls);
+        if (r > 0)
+            Gen(l, r - 1, s + ')', ls);
+    }
+}
